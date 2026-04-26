9ecd399f090b89880049e9369bb246da6ad48232
Piw01 <amfibloods@gmail.com>
2026-04-26 12:54:41 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\AlatController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\TransaksiController;
-use App\Http\Controllers\HomeController; // Tambahkan ini di bagian atas
+use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-// Rute otomatis untuk CRUD (index, create, store, edit, update, destroy)
+// Route otomatis CRUD (index, create, store, edit, update, destroy)
 Route::resource('kategori', KategoriController::class);
 Route::resource('alat', AlatController::class);
 Route::resource('pelanggan', PelangganController::class);
